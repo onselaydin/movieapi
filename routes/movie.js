06c3bf5ac83e87a7f72eacd67d741b01b2d22a96
unknown <onselaydin@gmail.com>
2019-01-28 16:00:08 +0300

    restful update delete insert top10 between

--- routes/movie.js
+++ routes/movie.js
@@ -1,13 +1,82 @@
 let express = require('express');
 let router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
-  //res.send('respond with a resource');
-  res.json({status:1});
+const Movie = require('../models/Movie')
+
+router.get('/', (req, res) => {
+  const promise = Movie.find({ });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
 });
 
-const Movie = require('../models/Movie')
+router.get('/top10', (req, res) => {
+  const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1 });
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/between/:start_year/:end_year', (req, res) => {
+  const { start_year, end_year } = req.params;
+  const promise = Movie.find(
+    { 
+    year: { "$gte": parseInt(start_year), "$lte": parseInt(end_year) }
+    }
+  );
+  promise.then((data) => {
+    res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+
+router.get('/:movie_id',(req, res, next) => {
+  //res.send(req.params.movie_id);
+  const promise = Movie.findById(req.params.movie_id);
+  promise.then((movie) => {
+    if(!movie)
+    next({ message: 'movie was not found!!'});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {
+      new: true
+    }
+  );
+  promise.then((movie) => {
+    if(!movie)
+    next({ message: 'movie was not found!!'});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = Movie.findByIdAndRemove(
+    req.params.movie_id
+  );
+  promise.then((movie) => {
+    if(!movie)
+    next({ message: 'movie was not found!!'});
+    res.json(movie);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
 
 router.post('/',(req,res,next) => {
   //const {title, imdb_score, category, country, year } = req.body; //1. yöntem
