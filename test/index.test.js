8a7dd29a34d2818152b4723c6d261116e5a7b667
unknown <onselaydin@gmail.com>
2019-02-07 14:25:39 +0300

    mocha test işlemleri

---
+++ test/index.test.js
@@ -0,0 +1,23 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const server = require('../app');
+
+chai.use(chaiHttp);
+
+describe(' Node Server ', () => {
+    it('(GET /) anasayfayı döndürür',(done) => {
+        chai.request(server).get('/').end((err, res) => {
+            res.should.have.status(200); // response return 200 olmalı
+            done();
+        });
+       
+    });
+    it('(GET /) movies döndürür',(done) => {
+        done();
+    });
+    it('(GET /) kullanıcıları döndürür',(done) => {
+        done();
+    });
+
+});
