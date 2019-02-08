3cabf98cdeac494dbca68ecdd8d8a427c6d7c568
unknown <onselaydin@gmail.com>
2019-02-08 13:10:48 +0300

    code - github - travis - heroku

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/Users');
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Hknyclcll naber :) işte heroku...' });
+  res.render('index', { title: 'code > github > travis > heroku' });
 });
 
 router.post('/register', (req, res, next) => {
